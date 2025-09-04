6a22e6c81885c70d08b1df8ec69f71637b849e8c
joeyk <joeybk85@gmail.com>
2025-09-03 19:30:09 -1000

    made the extension check case insensitive

--- src/entrypoints/ThumbnailPreview.tsx
+++ src/entrypoints/ThumbnailPreview.tsx
@@ -16,7 +16,7 @@ function getValueByPath<T>(obj: T, path: string): any {
 export default function ThumbnailPreview({ ctx }: PropTypes) {
    const value = getValueByPath(ctx.formValues, ctx.fieldPath);
 
-  if(value && value.startsWith('https') && (value.endsWith('.jpg') || value.endsWith('.jpeg') || value.endsWith('.png') || value.endsWith('.gif') || value.endsWith('.webp') || value.endsWith('.svg'))) {
+  if(value && value.toLowerCase().startsWith('https') && (value.toLowerCase().endsWith('.jpg') || value.toLowerCase().endsWith('.jpeg') || value.toLowerCase().endsWith('.png') || value.toLowerCase().endsWith('.gif') || value.toLowerCase().endsWith('.webp') || value.toLowerCase().endsWith('.svg'))) {
     return (
         <Canvas ctx={ctx}>
             <img src={value} alt="Thumbnail Preview" style={{ maxWidth: '100%', maxHeight: '160px', objectFit: 'contain', margin: '0 auto', display: 'block' }} />
